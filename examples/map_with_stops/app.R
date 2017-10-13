d8e85b70909f42abfc6e7709d32f794d30ab1059
Bartosz M. Kolasa <bkolasa@pilab.pl>
2017-10-13 14:40:13 +0200

    Dodatkowe częsci prezentacji

--- examples/map_with_stops/app.R
+++ examples/map_with_stops/app.R
@@ -9,17 +9,18 @@ source("global.R", local = FALSE)
 tbl(vertica, "mpk_pos") -> mpk_pos
 mpk_pos %>% distinct(lineno) %>% arrange(lineno) %>% collect -> lines
 
+
 tbl(vertica,"stops") %>% collect -> stops
 
 showStopDetails <- function(stopno_arg, lat,long){
   gsub("\"","",stopno_arg) -> stopno_arg
-  print(stopno_arg)
   mpk_pos %>% filter(stopno==stopno_arg) %>%group_by(lineno) %>% tally %>% 
     filter(n>1000) %>% arrange(lineno) %>% collect -> lines
-  print(lines)
   lines_str <- paste(lines$lineno, collapse = ",")
+  stop_name <- filter(stops, stopno==stopno_arg)$name
+  print(stop_name)
   tagList(
-    tags$h3(stopno_arg),
+    tags$h3(stop_name),
     tags$br(),
     paste0("Linie na przystanku:\n", strwrap(lines_str,width=10))
   ) %>% as.character ->content
